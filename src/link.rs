6eaa7034a4cb58958af06e05660075dc89706bfc
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2023-10-17 17:26:30 -0300

    big size update

---
+++ src/link.rs
@@ -0,0 +1,88 @@
+use std::str::FromStr;
+use teloxide::{prelude::*, types::MessageEntityKind};
+use url::Url;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Link {
+    X(Url),
+    Instagram(Url),
+    TikTok(Url),
+    Unsupported,
+}
+
+impl FromStr for Link {
+    type Err = url::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Url::from_str(s)?.into())
+    }
+}
+
+impl From<Url> for Link {
+    fn from(value: Url) -> Self {
+        let host = value.host().map(|host| host.to_string());
+        match host.as_deref() {
+            Some("twitter.com" | "x.com" | "www.twitter.com" | "www.x.com") => Self::X(value),
+            Some("instagram.com" | "www.instagram.com") => Self::Instagram(value),
+            Some("tiktok.com" | "www.tiktok.com" | "vm.tiktok.com") => Self::TikTok(value),
+            _ => Self::Unsupported,
+        }
+    }
+}
+
+impl Link {
+    /// Changes the host of the Url to one of the mapping alternatives
+    pub fn into_mapped_url(self) -> Option<Url> {
+        match self {
+            Self::X(mut url) => {
+                url.set_host(Some("fixupx.com")).unwrap();
+                url.set_query(None);
+                Some(url)
+            }
+            Self::Instagram(mut url) => {
+                url.set_host(Some("ddinstagram.com")).unwrap();
+                url.set_query(None);
+                Some(url)
+            }
+            Self::TikTok(mut url) => {
+                url.set_host(Some("vm.vxtiktok.com")).unwrap();
+                url.set_query(None);
+                Some(url)
+            }
+            Self::Unsupported => None,
+        }
+    }
+}
+
+/// Return a better version of the link for all links in the message
+pub fn map_links(msg: &Message) -> Vec<String> {
+    let links = links_from_msg(msg);
+    if !links.is_empty() {
+        let user = msg.from().map_or(&None, |user| &user.username);
+        println!("New message: {user:?}: {:?}", msg.text());
+    }
+
+    links
+        .into_iter()
+        .filter_map(Link::into_mapped_url)
+        .map(|url| {
+            let new_url = url.to_string();
+            println!("New link: {new_url:?}");
+            new_url
+        })
+        .collect()
+}
+
+/// Extract all links from a Teloxide's [`Message`].
+fn links_from_msg(msg: &Message) -> Vec<Link> {
+    let entities = msg.parse_entities().unwrap_or_default();
+    entities
+        .iter()
+        .filter_map(|entity| match entity.kind() {
+            MessageEntityKind::Url => entity.text().parse().ok(),
+            MessageEntityKind::TextLink { url, .. } => Some(url.clone().into()),
+            // Other message kinds are ignored
+            _ => None,
+        })
+        .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,10 @@
 #![deny(missing_docs)]
 #![deny(missing_doc_code_examples)]
 
-use teloxide::{prelude::*, types::MessageEntityKind};
+use futures::future::try_join_all;
+use teloxide::prelude::*;
 
-fn links_from_msg(msg: &Message) -> Vec<&str> {
-    let entities = msg.parse_entities().unwrap_or_default();
-    entities
-        .iter()
-        .filter_map(|entity| match entity.kind() {
-            MessageEntityKind::Url => Some(entity.text()),
-            MessageEntityKind::TextLink { url, .. } => Some(url.as_str()),
-            _ => None,
-        })
-        .collect()
-}
-
-fn map_link(link: &str) -> Option<String> {
-    match link {
-        _ if link.contains("twitter.com") && !link.contains("fxtwitter") => {
-            Some(link.replace("twitter.com", "fxtwitter.com"))
-        }
-        _ if link.contains("x.com") && !link.contains("fixupx") => {
-            Some(link.replace("x.com", "fixupx.com"))
-        }
-        _ if link.contains("tiktok.com") && !link.contains("vxtiktok") => {
-            Some(link.replace("tiktok.com", "vxtiktok.com"))
-        }
-        _ if link.contains("instagram.com") && !link.contains("ddinstagram") => {
-            let output_link = link.replace("instagram.com", "ddinstagram.com");
-            output_link.split("igshid=").next().map(str::to_string)
-        }
-
-        _ => None,
-    }
-}
+mod link;
 
 #[tokio::main]
 async fn main() {
@@ -51,17 +22,14 @@ async fn main() {
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        let links = links_from_msg(&msg);
-        if !links.is_empty() {
-            dbg!(msg.from().map(|user| &user.username), msg.text());
-            let new_links = links.iter().filter_map(|link| map_link(link));
-            for new_link in new_links {
-                bot.send_message(msg.chat.id, new_link)
-                    .allow_sending_without_reply(false)
-                    .reply_to_message_id(msg.id)
-                    .await?;
-            }
-        }
+        let new_links = link::map_links(&msg);
+        let handles = new_links.into_iter().map(|new_link| async {
+            bot.send_message(msg.chat.id, new_link)
+                .allow_sending_without_reply(false)
+                .reply_to_message_id(msg.id)
+                .await
+        });
+        try_join_all(handles).await?;
         Ok(())
     })
     .await;
