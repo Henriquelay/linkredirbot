f203ea2172e74ed2fcbfc9b398532a4fb701f86c
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2025-08-26 17:30:20 -0300

    nix flake and cargo lock

---
+++ flake.nix
@@ -0,0 +1,87 @@
+# from https://github.com/cpu/rust-flake
+
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          # Package dependencies
+          runtimeDeps = with pkgs; [ ];
+          buildDeps = with pkgs; [
+            # pkg-config # used with -sys crates
+            rustPlatform.bindgenHook
+          ];
+          devDeps = with pkgs; [
+            gdb
+            rust-analyzer
+            taplo # TOML toolkit
+            cargo-nextest # cool test runner
+          ];
+
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          msrv = cargoToml.package.rust-version;
+
+          rustPackage =
+            features:
+            (pkgs.makeRustPlatform {
+              cargo = pkgs.rust-bin.stable.latest.minimal;
+              rustc = pkgs.rust-bin.stable.latest.minimal;
+            }).buildRustPackage
+              {
+                inherit (cargoToml.package) name version;
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
+                buildFeatures = features;
+                buildInputs = runtimeDeps;
+                nativeBuildInputs = buildDeps;
+                # Uncomment if cargo tests require networking or otherwise
+                # don't play nicely with the Nix build sandbox:
+                # doCheck = false;
+              };
+
+          mkDevShell =
+            rustc:
+            pkgs.mkShell {
+              shellHook = ''
+                export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+              '';
+              buildInputs = runtimeDeps;
+              nativeBuildInputs = buildDeps ++ devDeps ++ [ rustc ];
+            };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+          };
+
+          packages.default = self'.packages.linkredirbot;
+          devShells.default = self'.devShells.stable;
+
+          # No features used so far
+          packages.linkredirbot = (rustPackage "");
+
+          devShells.nightly = (
+            mkDevShell (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+          );
+          devShells.stable = (mkDevShell pkgs.rust-bin.stable.latest.default);
+          devShells.msrv = (mkDevShell pkgs.rust-bin.stable.${msrv}.default);
+        };
+    };
+}
